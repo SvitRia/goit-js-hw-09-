799ba855edf9b3738d9956a627b55dd8dab42419
SvitRia <129522432+SvitRia@users.noreply.github.com>
2023-08-15 15:39:13 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body')
+console.log(btnStart);
+console.log(btnStop);
 
+btnStart.addEventListener('click', handlerStart)
+
+
+function handlerStart() {
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()}, 1000);
+        btnStart.disabled = true;
+}
+
+btnStop.addEventListener('click', handlerStop)
+
+function handlerStop() {
+    clearInterval(timerId);
+    btnStart.disable = false;
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }

df8cebeb69277463d38201a73e209bb92cba30a1
SvitRia <129522432+SvitRia@users.noreply.github.com>
2023-08-21 15:20:17 +0000

    Deploying to gh-pages from @ SvitRia/goit-js-hw-09-@12c53c6a00d5d7a9445ca9eba4f72666a84e84a0 🚀

---
+++ 01-color-switcher.184cee5e.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body");e.disabled=!0,t.addEventListener("click",(function(){setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3);t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(timerId),a.style.backgroundColor="#fafafa",t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.184cee5e.js.map
